1ade04e6a10686fed0294e8e9238a15b0bf088c4
a7chax <akuarung83@gmail.com>
2026-01-04 08:01:32 +0700

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,8 +11,20 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 9
 config.color_scheme = 'Breeze (Gogh)'
+config.use_fancy_tab_bar = true
+config.default_cursor_style = 'SteadyBlock'
+
+config.visual_bell = {
+    fade_in_function = 'EaseIn',
+    fade_in_duration_ms = 150,
+    fade_out_function = 'EaseOut',
+    fade_out_duration_ms = 150,
+}
+config.colors = {
+    visual_bell = '#202020',
+}
 
 -- Finally, return the configuration to wezterm:
 return config
