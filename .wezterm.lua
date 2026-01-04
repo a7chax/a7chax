5db405f10493fa793101461f8f786dca460d2d1d
a7chax <akuarung83@gmail.com>
2026-01-05 01:59:28 +0700

    Enable scrollbar and add layered background

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,7 @@ config.initial_rows = 28
 -- or, changing the font size and color scheme.
 config.font_size = 10
 config.color_scheme = 'Breeze (Gogh)'
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.default_cursor_style = 'SteadyBlock'
 
 config.visual_bell = {
@@ -25,6 +25,89 @@ config.visual_bell = {
 config.colors = {
     visual_bell = '#202020',
 }
+
+config.notification_handling = 'AlwaysShow'
+
+config.enable_scroll_bar = true
+config.min_scroll_bar_height = '2cell'
+config.colors = {
+  scrollbar_thumb = 'white',
+}
+config.background = {
+  -- This is the deepest/back-most layer. It will be rendered first
+  {
+    source = {
+      File = '/home/nevermind/.wezterm_bg/Backgrounds/spaceship_bg_1.png',
+    },
+    -- The texture tiles vertically but not horizontally.
+    -- When we repeat it, mirror it so that it appears "more seamless".
+    -- An alternative to this is to set `width = "100%"` and have
+    -- it stretch across the display
+    repeat_x = 'Mirror',
+    hsb = dimmer,
+    -- When the viewport scrolls, move this layer 10% of the number of
+    -- pixels moved by the main viewport. This makes it appear to be
+    -- further behind the text.
+    attachment = { Parallax = 0.1 },
+  },
+  -- Subsequent layers are rendered over the top of each other
+  {
+    source = {
+      File = '/home/nevermind/.wezterm_bg/Overlays/overlay_1_spines.png',
+    },
+    width = '100%',
+    repeat_x = 'NoRepeat',
+
+    -- position the spins starting at the bottom, and repeating every
+    -- two screens.
+    vertical_align = 'Bottom',
+    repeat_y_size = '200%',
+    hsb = dimmer,
+
+    -- The parallax factor is higher than the background layer, so this
+    -- one will appear to be closer when we scroll
+    attachment = { Parallax = 0.2 },
+  },
+  {
+    source = {
+      File = '/home/nevermind/.wezterm_bg/Overlays/overlay_2_alienball.png',
+    },
+    width = '100%',
+    repeat_x = 'NoRepeat',
+
+    -- start at 10% of the screen and repeat every 2 screens
+    vertical_offset = '10%',
+    repeat_y_size = '200%',
+    hsb = dimmer,
+    attachment = { Parallax = 0.3 },
+  },
+  {
+    source = {
+      File = '/home/nevermind/.wezterm_bg/Overlays/overlay_3_lobster.png',
+    },
+    width = '100%',
+    repeat_x = 'NoRepeat',
+
+    vertical_offset = '30%',
+    repeat_y_size = '200%',
+    hsb = dimmer,
+    attachment = { Parallax = 0.4 },
+  },
+  {
+    source = {
+      File = '/home/nevermind/.wezterm_bg/Overlays/overlay_4_spiderlegs.png',
+    },
+    width = '100%',
+    repeat_x = 'NoRepeat',
+
+    vertical_offset = '50%',
+    repeat_y_size = '150%',
+    hsb = dimmer,
+    attachment = { Parallax = 0.5 },
+  },
+}
+
+config.automatically_reload_config = true
 
 -- Finally, return the configuration to wezterm:
 return config
