41daf5916128cfbd40551f50224b17a12acbaf42
a7chax <akuarung83@gmail.com>
2026-01-06 13:10:30 +0700

    Customize WezTerm UI and custom tab titles
    Set window_decorations to RESIZE, reduce font_size to 9, and disable the
    fancy tab bar. Place the tab bar at the bottom, hide the new-tab button,
    and cap tab width to 40. Add window reload/resize handlers to track
    column size and implement a custom format-tab-title that shows the tab
    index and current directory.

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,13 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+
+config.window_decorations = "RESIZE"
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 9
 config.color_scheme = 'Breeze (Gogh)'
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.default_cursor_style = 'SteadyBlock'
 
 config.visual_bell = {
@@ -28,7 +28,63 @@ config.colors = {
 
 config.notification_handling = 'AlwaysShow'
 
+
+
 config.automatically_reload_config = true
+
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+
+function get_max_cols(window)
+    local tab = window:active_tab()
+    local cols = tab:get_size().cols
+    return cols
+end
+
+wezterm.on(
+    'window-config-reloaded',
+    function(window)
+        wezterm.GLOBAL.cols = get_max_cols(window)
+    end
+)
+
+wezterm.on(
+    'window-resized',
+    function(window, pane)
+        wezterm.GLOBAL.cols = get_max_cols(window)
+    end
+)
+
+wezterm.on(
+    'format-tab-title',
+    function(tab)
+        local pane = tab.active_pane
+        local cwd_uri = pane.current_working_dir
+        local title = '?'
+
+        if cwd_uri then
+            local cwd = cwd_uri.file_path or tostring(cwd_uri)
+            cwd = cwd:gsub('^file://', '')
+            cwd = cwd:gsub('/+$', '')
+            title = cwd:match('([^/]+)$') or cwd
+        end
+
+        return {
+            { Text = '  [' .. (tab.tab_index + 1) .. '] ' .. title .. '  ' },
+        }
+    end
+)
+
+if title == wezterm.home_dir:match('([^/]+)$') then
+    title = '~'
+end
+
+
+
+-- The filled in variant of the < symbol
+config.tab_max_width = 40
+
+
 
 -- Finally, return the configuration to wezterm:
 return config
