7bfa77121f57416f96ca0dbeca8563c3774a4747
a7chax <akuarung83@gmail.com>
2026-01-08 00:21:13 +0700

    Enhance wezterm tab title formatting

--- .wezterm.lua
+++ .wezterm.lua
@@ -29,7 +29,7 @@ config.colors = {
 config.notification_handling = 'AlwaysShow'
 config.scrollback_lines = 1000
 config.front_end = "OpenGL"
-config.webgpu_power_preference="HighPerformance"
+config.webgpu_power_preference = "HighPerformance"
 
 
 config.automatically_reload_config = true
@@ -57,25 +57,67 @@ wezterm.on(
     end
 )
 
-wezterm.on(
-    'format-tab-title',
-    function(tab)
-        local pane = tab.active_pane
-        local cwd_uri = pane.current_working_dir
-        local title = '?'
+local wezterm           = require 'wezterm'
 
-        if cwd_uri then
-            local cwd = cwd_uri.file_path or tostring(cwd_uri)
-            cwd = cwd:gsub('^file://', '')
-            cwd = cwd:gsub('/+$', '')
-            title = cwd:match('([^/]+)$') or cwd
-        end
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
-        return {
-            { Text = '  [' .. (tab.tab_index + 1) .. '] ' .. title .. '  ' },
-        }
+-- Colors (sesuaikan theme kamu)
+local ACTIVE_BG         = '#268bd2'
+local ACTIVE_FG         = '#ffffff'
+local INACTIVE_BG       = '#1e1e1e'
+local INACTIVE_FG       = '#9aa0a6'
+local HOVER_BG          = '#2a2a2a'
+local HOVER_FG          = '#ffffff'
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local pane = tab.active_pane
+    local title = ' ? '
+
+    if pane.current_working_dir then
+        local cwd = pane.current_working_dir.file_path or tostring(pane.current_working_dir)
+        cwd = cwd:gsub('^file://', ''):gsub('/+$', '')
+        title = cwd:match('([^/]+)$') or cwd
     end
-)
+
+    -- home → ~
+    if title == wezterm.home_dir:match('([^/]+)$') then
+        title = '~'
+    end
+
+    local index = tab.tab_index + 1
+    local text = string.format('  %d  %s  ', index, title)
+
+    -- truncate aman
+    if #text > max_width then
+        text = wezterm.truncate_right(text, max_width - 2) .. '… '
+    end
+
+    local bg, fg
+
+    if tab.is_active then
+        bg = ACTIVE_BG
+        fg = ACTIVE_FG
+    elseif hover then
+        bg = HOVER_BG
+        fg = HOVER_FG
+    else
+        bg = INACTIVE_BG
+        fg = INACTIVE_FG
+    end
+
+    return {
+        -- tab body
+        { Background = { Color = bg } },
+        { Foreground = { Color = fg } },
+        { Text = text },
+
+        -- powerline separator
+        { Foreground = { Color = bg } },
+        { Text = SOLID_RIGHT_ARROW },
+
+    }
+end)
+
 
 if title == wezterm.home_dir:match('([^/]+)$') then
     title = '~'
